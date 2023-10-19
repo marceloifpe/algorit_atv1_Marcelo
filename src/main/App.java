1244bd32c5a64dc91d0e0720b22906e715a12300
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2023-10-19 20:34:36 +0000

    teste 2

--- src/main/App.java
+++ src/main/App.java
@@ -1,15 +1,60 @@
 package main;
 public class App {
-    public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
-        int a = 0
-    }
+//     public static int encontre(int[] arrayOrdenado, int num){
+//         return -1;
+//         //int a == 0;
+//     }
 
-    public static int maior(int[] arrayDesordenado){
-        return -1;
-    }
+//     public static int maior(int[] arrayDesordenado){
+//         return -1;
+//     }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
-    }
-}
+//     public static Integer parMaisProximo(Integer[][] pares){
+//         return 1;
+//     }
+// }
+public static void main(String[] args) throws Exception {
+    //         int[] array = { 1, 3, 5, 7, 9 };
+    //         int valorProcurado = 7;
+    //         int resultado = encontre(array, valorProcurado);
+    
+    //         if (resultado == 1) {
+    //             System.out.println("Valor encontrado no array." + resultado);
+    //         } else {
+    //             System.out.println("Valor não encontrado no array." + resultado);
+    //         }
+    //     }
+    
+    //     public static int encontre(int[] array, int n) {
+    //         for (int i = 0; i < array.length; i++) {
+    //             if (array[i] == n) {
+    //                 return 1;
+    //             }
+    //             if (array[i] > n) {
+    //                 return 0;
+    //             }
+    //         }
+    
+    //         return 0;
+    //     }
+    // }
+        //int maximo = 0;
+       // int[] array = {5, 2, 30, 4, 5};
+        //int[] array = {5, 2, 9, 3, 6, 1, 8};
+        // for(int i = 0; i < array.length; i++){
+        //     if (maximo < array[i]){
+        //         maximo = array[i];
+                
+     //       }  
+       // }
+       // System.out.println("Maior numero da lista " + maximo);
+       
+        int[][] pontos = new int[5][2];
+            pontos
+            pontos
+            pontos
+            pontos
+            pontos
+            for
+       }
+    }}
