b399b8108c765b2aeec4fbc0e172284df6c21579
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2023-10-19 19:21:55 +0000

    alterando 1

--- src/main/App.java
+++ src/main/App.java
@@ -2,6 +2,7 @@ package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
         return -1;
+        int a = 0
     }
 
     public static int maior(int[] arrayDesordenado){
