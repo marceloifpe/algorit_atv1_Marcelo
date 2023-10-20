ca3fe2fef75e5a08298c4f766969c826285970d6
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2023-10-20 14:24:03 +0000

    pontomaisproximo teste

--- src/main/App.java
+++ src/main/App.java
@@ -1,60 +1,99 @@
 package main;
+
 public class App {
-//     public static int encontre(int[] arrayOrdenado, int num){
-//         return -1;
-//         //int a == 0;
-//     }
 
-//     public static int maior(int[] arrayDesordenado){
-//         return -1;
-//     }
+    public static int encontre(int[] arrayOrdenado, int num) {
+        for (int i = 0; i < arrayOrdenado.length; i++) {
+            if (arrayOrdenado[i] == num) {
+                return 1;
+            }
+            if (arrayOrdenado[i] > num) {
+                return 0;
+            }
+        }
+        return 0;
+    }
 
-//     public static Integer parMaisProximo(Integer[][] pares){
-//         return 1;
-//     }
-// }
-public static void main(String[] args) throws Exception {
-    //         int[] array = { 1, 3, 5, 7, 9 };
-    //         int valorProcurado = 7;
-    //         int resultado = encontre(array, valorProcurado);
-    
-    //         if (resultado == 1) {
-    //             System.out.println("Valor encontrado no array." + resultado);
-    //         } else {
-    //             System.out.println("Valor não encontrado no array." + resultado);
-    //         }
-    //     }
-    
-    //     public static int encontre(int[] array, int n) {
-    //         for (int i = 0; i < array.length; i++) {
-    //             if (array[i] == n) {
-    //                 return 1;
-    //             }
-    //             if (array[i] > n) {
-    //                 return 0;
-    //             }
-    //         }
-    
-    //         return 0;
-    //     }
-    // }
-        //int maximo = 0;
-       // int[] array = {5, 2, 30, 4, 5};
-        //int[] array = {5, 2, 9, 3, 6, 1, 8};
-        // for(int i = 0; i < array.length; i++){
-        //     if (maximo < array[i]){
-        //         maximo = array[i];
-                
-     //       }  
-       // }
-       // System.out.println("Maior numero da lista " + maximo);
-       
-        int[][] pontos = new int[5][2];
-            pontos
-            pontos
-            pontos
-            pontos
-            pontos
-            for
-       }
-    }}
+    public static int maior(int[] arrayDesordenado) {
+        if (arrayDesordenado.length == 0) {
+            return -1; // Retorna -1 se o array estiver vazio.
+        }
+
+        int max = arrayDesordenado[0];
+        for (int i = 1; i < arrayDesordenado.length; i++) {
+            if (arrayDesordenado[i] > max) {
+                max = arrayDesordenado[i];
+            }
+        }
+
+        return max;
+    }
+    public static Double parMaisProximo(Integer[][] pares) {
+        if (pares.length < 2) {
+            return null; // Retorna null se não houver pelo menos 2 pontos.
+        }
+
+        double distanciaMinima = Double.MAX_VALUE;
+
+        for (int i = 0; i < pares.length; i++) {
+            for (int j = i + 1; j < pares.length; j++) {
+                int x1 = pares[i][0];
+                int y1 = pares[i][1];
+                int x2 = pares[j][0];
+                int y2 = pares[j][1];
+
+                double distancia = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+                if (distancia < distanciaMinima) {
+                    distanciaMinima = distancia;
+                }
+            }
+        }
+
+        return distanciaMinima;
+    }
+
+
+    public static void main(String[] args) {
+        // Exemplo de teste do método encontre
+        //teste1
+        int[] arrayOrdenado = {1, 2, 3, 4, 5};
+        int valorProcurado = 3;
+        //teste2
+        //  int[] arrayOrdenado = {1, 2, 3};
+        // int valorProcurado = 3;
+           //teste3
+        //  int[] arrayOrdenado = {1, 2, 3};
+        // int valorProcurado = 7;
+           //teste4
+        //  int[] arrayOrdenado = {1, 2, 7, 3};
+        // int valorProcurado = 7;
+           //teste5
+        //  int[] arrayOrdenado = {};
+        // int valorProcurado = 7;
+        int resultadoEncontre = encontre(arrayOrdenado, valorProcurado);
+
+        if (resultadoEncontre == 1) {
+            System.out.println("\n Valor encontrado na lista: " + valorProcurado);
+        } else {
+            System.out.println("\n Valor não encontrado na lista: ");
+        }
+
+        // método maior
+        int[] arrayDesordenado = {5, 2, 30, 4, 5};
+        int resultadoMaior = maior(arrayDesordenado);
+        System.out.println("Maior número da lista: " + resultadoMaior);
+
+                // Teste do método parMaisProximo
+        Integer[][] pontos = {
+            {2, 4},
+            {5, 9},
+            {1, 3},
+            {8, 2},
+            {6, 7}
+        };
+        Double resultadoParMaisProximo = parMaisProximo(pontos);
+        System.out.println("Resultado do parMaisProximo: " + resultadoParMaisProximo);
+    }
+}
+
--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -1,19 +1,19 @@
 // package test;
 
-
 // import static org.junit.Assert.assertEquals;
 // import static org.junit.Assert.assertNull;
 
 // import org.junit.Test;
 
-// import main.App;
+// import main.App; // Importe a classe App
+
 // public class AppTest {
 //     @Test
 //     public void testEncontre() {
 //         assertEquals(1, App.encontre(new int[]{1, 2, 3, 4, 5}, 3));
 //         assertEquals(1, App.encontre(new int[]{1, 2, 3}, 3));
 //         assertEquals(0, App.encontre(new int[]{1, 2, 3}, 7));
-//         assertEquals(1, App.encontre(new int[]{1, 2,7, 3}, 7));
+//         assertEquals(1, App.encontre(new int[]{1, 2, 7, 3}, 7));
 //         assertEquals(0, App.encontre(new int[]{}, 7));
 //     }
 
@@ -30,6 +30,6 @@
 //     public void testParMaisProximo() {
 //         assertEquals(1.414, App.parMaisProximo(new Integer[][]{new Integer[]{2, 4}, new Integer[]{5, 9}, new Integer[]{1, 3}, new Integer[]{8, 2}, new Integer[]{6, 7}}));
 //         assertNull(App.parMaisProximo(new Integer[][]{}));
-//         assertEquals(0, App.parMaisProximo(new Integer[][]{new Integer[]{2, 4}, new Integer[]{5, 9}, new Integer[]{1, 3}, new Integer[]{8, 2}, new Integer[]{2, 4}}));
+//         assertEquals(2.0, App.parMaisProximo(new Integer[][]{new Integer[]{2, 4}, new Integer[]{5, 9}, new Integer[]{1, 3}, new Integer[]{8, 2}, new Integer[]{2, 4}}));
 //     }
 // }
